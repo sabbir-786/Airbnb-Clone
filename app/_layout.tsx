ee08f544c0a1cc2b131cb9806b2daa61e93c2230
sabbir-786 <sabbir.ansari3574@gmail.com>
2024-11-23 20:56:33 +0530

    Resolved merge conflicts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,8 +23,10 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    'mon' : require('../assets/fonts/Montserrat-Regular.ttf'),
+    'mon-sb' : require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'mon-b' : require('../assets/fonts/Montserrat-Bold.ttf'),
+    'mon-m' : require('../assets/fonts/Montserrat-Medium.ttf'),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -46,14 +48,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
-    </ThemeProvider>
   );
 }
